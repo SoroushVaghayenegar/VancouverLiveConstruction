7f3b5dee41b35183e5fbdac33510aab4c5ecdb30
ryffvy <rollerr3@gmail.com>
2015-04-01 15:17:15 -0700

    Still fixing
    Need to commit to push to heroku

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,7 @@
 $(function(){
     $(".deleteIPConstr").click(function(){
         var elem_t = $(this).parent();
+        $(this).prop('disabled', true);
         $.ajax({
             url: '/delete_constr/',
             type: 'POST',
@@ -15,6 +16,7 @@ $(function(){
 $(function(){
     $(".deleteUPConstr").click(function(){
         var elem_t = $(this).parent();
+        $(this).prop('disabled', true);
         $.ajax({
             url: '/delete_constr/',
             type: 'POST',
@@ -29,6 +31,7 @@ $(function(){
 $(function(){
     $(".deleteRCConstr").click(function(){
         var elem_t = $(this).parent();
+        $(this).prop('disabled', true);
         $.ajax({
             url: '/delete_constr/',
             type: 'POST',
@@ -53,10 +56,13 @@ $(function(){
 $(function(){
     $(".load_btn").click(function(){
         $('#loading').css('display', 'inline');
+        $(this).prop('disabled', true);
         $.ajax({
             url: '/load_data/',
             type: 'POST',
-            success: location.reload()
+            success: function(){
+                location.reload();
+            }
         });
     });
     $('#loading').css('display', 'none');
